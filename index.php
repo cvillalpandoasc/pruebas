e5f6ac479173084875224fcbfd7b381cea076964
Carlos Villalpando <cv9019@gmail.com>
2016-03-29 13:06:03 -0500

    Template Prueba 1

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>ASCC</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Open+Sans" />
+    <link rel="stylesheet" type="text/css" href="estilo.css">
+  </head>
+  <body >
+    <div class="head">
+      <div class="bodyPage">
+        <div class="imgHead"></div>
+        <h3 class="titleHead">Estaciones de aprendizaje</h3>
+      </div>
+    </div>
+    <div class="bodyPage" style="margin-top: 50px;height: 90vh; overflow: scroll;">
+      <table class="tablaCentral" cellpadding="0" cellspacing="0" border="0">
+        <tr>
+          <td rowspan="4" class="fondoOscuro colorBorder" width="10%">
+            <div class="img1 img"></div>
+            <div class="img2 img"></div>
+            <div class="img3 img"></div>
+          </td>
+          <td rowspan="4" class="fondoMedio colorBorder" width="10%">
+            <div class="vertical-text text-white text-center">Interdisciplinariedad</div>
+          </td>
+          <th colspan="2" class="fondoMedio colorBorder padding">Recursos</th>
+        </tr>
+        <tr>
+          <td colspan="2" class="fondoClaro colorBorder padding">2 Fuentes destacadas</td>
+        </tr>
+        <tr>
+          <th class="fondoMedio colorBorder padding" width="40%">Actividades</th>
+          <th class="fondoMedio colorBorder padding" width="40%">Producto</th>
+        </tr>
+        <tr>
+          <td class="fondoClaro colorBorder padding text-gray">
+            <ul>
+              <li>Elaboraci&oacute;n de dibujo (4 Elementos)</li>
+              <li>Lenado de esquema (4 Elementos)</li>
+              <li>Elaboraci&oacute;n de lista (5 Enunciados)</li>
+              <li>Contestaci&oacute;n a pregunta clave</li>
+            </ul>
+          </td>
+          <td class="fondoClaro colorBorder padding text-gray">
+            <div class="text-center">An&aacute;lisis de los efectos del medio ambiente probocados por el calentamiento global</div>
+            <ul>
+              <li>Dibujo(4 Elementos)</li>
+              <li>Esquema (4 Elementos)</li>
+              <li>Lista (5 Enunciados)</li>
+              <li>Respuesta a pregunta clave</li>
+            </ul>
+          </td>
+        </tr>
+      </table>
+    </div>
+    <div class="footerLeft"></div>
+    <div class="footerRight"></div>
+  </body>
+</html>
